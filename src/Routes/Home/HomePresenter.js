55f19deb9021adf51c167e8c6498b979361a4def
azerckid <75958755+azerckid@users.noreply.github.com>
2020-12-28 19:02:41 +0900

    ....

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,11 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import { ReactComponent as ReactLogo } from "../../assets/img/BancoGamesLogoCircle.svg";
 
 const Container = styled.div`
   width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const SvgTitleContainer = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  background-color: brown;
+`;
+
+const SvgTitle = styled.div`
+  width: 70vw;
+  padding: 100px;
+  background-color: brown;
+`;
+
+const StyledLogo = styled(ReactLogo)`
+  height: 25rem;
+  width: 25rem;
+  display: block;
+  margin: auto;
 `;
 
 const Section1 = styled.div`
@@ -71,6 +92,23 @@ const Section5 = styled.div`
 const HomePresenter = () => {
   return (
     <Container>
+      <SvgTitleContainer>
+        <SvgTitle>
+          <svg
+            id="Layer_1"
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 92.25 92.25"
+          >
+            <polygon points="33.04 59.14 59.22 59.14 59.22 51.19 36.17 51.19 36.17 41.07 59.22 41.07 59.22 33.12 33.04 33.12 33.04 59.14" />
+            <path
+              d="M297.67,340.46a46.13,46.13,0,1,0,46.12,46.13A46.13,46.13,0,0,0,297.67,340.46ZM321,401.76a2.72,2.72,0,0,1-.65,1.45l-6.18,6a1.61,1.61,0,0,1-1.36.65H274.3V363.3h38.55a1.6,1.6,0,0,1,1.36.64l6.18,6a2.72,2.72,0,0,1,.65,1.45Z"
+              transform="translate(-251.54 -340.46)"
+            />
+          </svg>
+        </SvgTitle>
+      </SvgTitleContainer>
+
       <Section1>
         <h1>Action Golf Title</h1>
         <p>액션골프 Typo와 캐릭터 이미지는 애니메이션</p>
